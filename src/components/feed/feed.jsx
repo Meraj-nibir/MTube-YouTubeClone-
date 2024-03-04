5cd4da09b29f738bcf85716e30bcbd6b5064e657
Meraj-nibir <merajurrahmanniber@gmail.com>
2024-03-04 15:52:32 +0600

    homePage.css & 55:14

--- src/components/feed/feed.jsx
+++ src/components/feed/feed.jsx
@@ -1,11 +1,57 @@
 import React from "react";
 import "./feed.css";
+import thumbnail1 from "../../assets/Thumbnail/GMT NEWS24.jpg";
 const Feed = ()=>{
     return(
-        <div className="card">
-            <img src="" alt="" />
-            <h2></h2>
-            <h3></h3>
+        <div className="feed">
+            <div className="card">
+                <img src={thumbnail1} alt="Video thumbnail" />
+                <h2>How to design a news banner</h2>
+                <h3>Banner_Creator</h3>
+                <p>2.5K Views &bull; 2 days ago</p>
+            </div>
+            <div className="card">
+                <img src={thumbnail1} alt="Video thumbnail" />
+                <h2>How to design a news banner</h2>
+                <h3>Banner_Creator</h3>
+                <p>2.5K Views &bull; 2 days ago</p>
+            </div>
+            <div className="card">
+                <img src={thumbnail1} alt="Video thumbnail" />
+                <h2>How to design a news banner</h2>
+                <h3>Banner_Creator</h3>
+                <p>2.5K Views &bull; 2 days ago</p>
+            </div>
+            <div className="card">
+                <img src={thumbnail1} alt="Video thumbnail" />
+                <h2>How to design a news banner</h2>
+                <h3>Banner_Creator</h3>
+                <p>2.5K Views &bull; 2 days ago</p>
+            </div>
+            <div className="card">
+                <img src={thumbnail1} alt="Video thumbnail" />
+                <h2>How to design a news banner</h2>
+                <h3>Banner_Creator</h3>
+                <p>2.5K Views &bull; 2 days ago</p>
+            </div>
+            <div className="card">
+                <img src={thumbnail1} alt="Video thumbnail" />
+                <h2>How to design a news banner</h2>
+                <h3>Banner_Creator</h3>
+                <p>2.5K Views &bull; 2 days ago</p>
+            </div>
+            <div className="card">
+                <img src={thumbnail1} alt="Video thumbnail" />
+                <h2>How to design a news banner</h2>
+                <h3>Banner_Creator</h3>
+                <p>2.5K Views &bull; 2 days ago</p>
+            </div>
+            <div className="card">
+                <img src={thumbnail1} alt="Video thumbnail" />
+                <h2>How to design a news banner</h2>
+                <h3>Banner_Creator</h3>
+                <p>2.5K Views &bull; 2 days ago</p>
+            </div>
         </div>
     )
 }
