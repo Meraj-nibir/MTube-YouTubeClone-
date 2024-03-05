c0ee98bc68ec37840cc1891aae1203938247cd3e
Meraj-nibir <merajurrahmanniber@gmail.com>
2024-03-05 15:26:33 +0600

    Feed er api kaj kore na & 2:2:00

--- src/components/feed/feed.jsx
+++ src/components/feed/feed.jsx
@@ -1,107 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./feed.css";
+import "../../data";
 import { Link } from "react-router-dom";
 import thumbnail1 from "../../assets/Thumbnail/GMT NEWS24.jpg";
+import { API_KEY } from "../../data";
+import moment from "moment";
 
-const Feed = ()=>{
+const Feed = (category)=>{
+
+    const [data,setData]=useState([]);
+
+    const fetchData = async()=>{
+        const videoList_URL=`https://youtube.googleapis.com/youtube/v3/videos?part=snippet.contentDetails.statistics&chart=mostPopular&maxResults=50&regionCode=BD&videoCategoryId=${category}&key=${API_KEY}`;
+        await fetch(videoList_URL).then(response=>response.json()).then(data=>setData(data.items));
+    }
+    useEffect(()=>{
+        fetchData();
+    },[category])
+
     return(
         <div className="feed">
-            <Link to={"video/20/4521"} className="card">
-                <img src={thumbnail1} alt="Video thumbnail" />
-                <h2>How to design a news banner</h2>
-                <h3>Banner_Creator</h3>
-                <p>2.5K Views &bull; 2 days ago</p>
-            </Link>
-            <div className="card">
-                <img src={thumbnail1} alt="Video thumbnail" />
-                <h2>How to design a news banner</h2>
-                <h3>Banner_Creator</h3>
-                <p>2.5K Views &bull; 2 days ago</p>
-            </div>
-            <div className="card">
-                <img src={thumbnail1} alt="Video thumbnail" />
-                <h2>How to design a news banner</h2>
-                <h3>Banner_Creator</h3>
-                <p>2.5K Views &bull; 2 days ago</p>
-            </div>
-            <div className="card">
-                <img src={thumbnail1} alt="Video thumbnail" />
-                <h2>How to design a news banner</h2>
-                <h3>Banner_Creator</h3>
-                <p>2.5K Views &bull; 2 days ago</p>
-            </div>
-            <div className="card">
-                <img src={thumbnail1} alt="Video thumbnail" />
-                <h2>How to design a news banner</h2>
-                <h3>Banner_Creator</h3>
-                <p>2.5K Views &bull; 2 days ago</p>
-            </div>
-            <div className="card">
-                <img src={thumbnail1} alt="Video thumbnail" />
-                <h2>How to design a news banner</h2>
-                <h3>Banner_Creator</h3>
-                <p>2.5K Views &bull; 2 days ago</p>
-            </div>
-            <div className="card">
-                <img src={thumbnail1} alt="Video thumbnail" />
-                <h2>How to design a news banner</h2>
-                <h3>Banner_Creator</h3>
-                <p>2.5K Views &bull; 2 days ago</p>
-            </div>
-            <div className="card">
-                <img src={thumbnail1} alt="Video thumbnail" />
-                <h2>How to design a news banner</h2>
-                <h3>Banner_Creator</h3>
-                <p>2.5K Views &bull; 2 days ago</p>
-            </div>
-            <div className="card">
-                <img src={thumbnail1} alt="Video thumbnail" />
-                <h2>How to design a news banner</h2>
-                <h3>Banner_Creator</h3>
-                <p>2.5K Views &bull; 2 days ago</p>
-            </div>
-            <div className="card">
-                <img src={thumbnail1} alt="Video thumbnail" />
-                <h2>How to design a news banner</h2>
-                <h3>Banner_Creator</h3>
-                <p>2.5K Views &bull; 2 days ago</p>
-            </div>
-            <div className="card">
-                <img src={thumbnail1} alt="Video thumbnail" />
-                <h2>How to design a news banner</h2>
-                <h3>Banner_Creator</h3>
-                <p>2.5K Views &bull; 2 days ago</p>
-            </div>
-            <div className="card">
-                <img src={thumbnail1} alt="Video thumbnail" />
-                <h2>How to design a news banner</h2>
-                <h3>Banner_Creator</h3>
-                <p>2.5K Views &bull; 2 days ago</p>
-            </div>
-            <div className="card">
-                <img src={thumbnail1} alt="Video thumbnail" />
-                <h2>How to design a news banner</h2>
-                <h3>Banner_Creator</h3>
-                <p>2.5K Views &bull; 2 days ago</p>
-            </div>
-            <div className="card">
-                <img src={thumbnail1} alt="Video thumbnail" />
-                <h2>How to design a news banner</h2>
-                <h3>Banner_Creator</h3>
-                <p>2.5K Views &bull; 2 days ago</p>
-            </div>
-            <div className="card">
-                <img src={thumbnail1} alt="Video thumbnail" />
-                <h2>How to design a news banner</h2>
-                <h3>Banner_Creator</h3>
-                <p>2.5K Views &bull; 2 days ago</p>
-            </div>
-            <div className="card">
-                <img src={thumbnail1} alt="Video thumbnail" />
-                <h2>How to design a news banner</h2>
-                <h3>Banner_Creator</h3>
-                <p>2.5K Views &bull; 2 days ago</p>
-            </div>
+            {data.map((item,index)=>{
+                return(
+                    <Link to={`video/${item.snippet.categoryId}/${item.id}`} className="card">
+                        <img src={item.snippet.thumbnails.medium.url} alt="Video thumbnail" />
+                        <h2>{item.snippet.title}</h2>
+                        <h3>{item.snippet.channelTitle}</h3>
+                        <p>{value_converter(item.statistics.viewCount)} Views &bull; {moment(item.snippet.publishedAt).fromNow()}</p>
+                    </Link>
+                )
+            })}
+            
         </div>
     )
 }
