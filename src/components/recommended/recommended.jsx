a9da150fdb7936c0f2b57a508f4f7b47af627ebb
Meraj-UR-Rahman Nibir <merajurrahmanniber@gmail.com>
2024-03-07 00:49:12 +0600

    Complete

--- src/components/recommended/recommended.jsx
+++ src/components/recommended/recommended.jsx
@@ -1,16 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import './recommended.css';
 import { API_KEY, value_converter } from '../../data';
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
 
 
-const Recommended = ({categoryId}) => {
+const Recommended = () => {
+  
+  const {categoryId} = useParams();
 
   const [apiData, setApiData] = useState(null);
 
-  const fetchRecomendedVideo = async()=>{
-    const recommendedVideo_url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2contentDetails%2statistics&chart=mostPopular&maxResults=50&regionCode=BD&videoCategoryId=${categoryId}&key=${API_KEY} `;
+  const fetchRecomendedVideo = async () => {
+    const recommendedVideo_url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=55&regionCode=BD&key=${API_KEY}`;
     await fetch(recommendedVideo_url)
       .then((res) => res.json())
       .then((data) => setApiData(data.items));
@@ -18,7 +20,7 @@ const Recommended = ({categoryId}) => {
 
   useEffect(() => {
     fetchRecomendedVideo();
-  }, []);
+  }, [categoryId]);
 
 
 
@@ -31,7 +33,7 @@ const Recommended = ({categoryId}) => {
           <div className="video-info">
             <h4>{item.snippet.title}</h4>
             <p>{item.snippet.channelTitle}</p>
-            <p>{value_converter(item.snippet.viewCount)} view</p>
+            <p>{value_converter(item.statistics.viewCount)} view</p>
           </div>
         </Link>
       ))}
