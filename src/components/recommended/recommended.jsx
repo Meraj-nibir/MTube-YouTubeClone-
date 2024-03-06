5f8bf52476a6469bf863cd05c68dd8fc84a8d50d
Meraj-nibir <merajurrahmanniber@gmail.com>
2024-03-06 13:21:19 +0600

    all done but recomended videos are not showing

--- src/components/recommended/recommended.jsx
+++ src/components/recommended/recommended.jsx
@@ -1,83 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './recommended.css';
-import thumbnail1 from "../../assets/Thumbnail/GMT NEWS24.jpg";
-const Recommended = () => {
+import { API_KEY, value_converter } from '../../data';
+import { Link } from 'react-router-dom';
+
+
+
+const Recommended = ({categoryId}) => {
+
+  const [apiData, setApiData] = useState(null);
+
+  const fetchRecomendedVideo = async()=>{
+    const recommendedVideo_url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2contentDetails%2statistics&chart=mostPopular&maxResults=50&regionCode=BD&videoCategoryId=${categoryId}&key=${API_KEY} `;
+    await fetch(recommendedVideo_url)
+      .then((res) => res.json())
+      .then((data) => setApiData(data.items));
+  }
+
+  useEffect(() => {
+    fetchRecomendedVideo();
+  }, []);
+
+
+
+
   return (
     <div className='recommended'>
-      <div className="side-video-list">
-        <img src={thumbnail1} alt="" />
-        <div className="video-info">
-          <h4>animated car</h4>
-          <p>Riolu</p>
-          <p>166k view</p>
-        </div>
-      </div>
-      <div className="side-video-list">
-        <img src={thumbnail1} alt="" />
-        <div className="video-info">
-          <h4>animated car</h4>
-          <p>Riolu</p>
-          <p>166k view</p>
-        </div>
-      </div>
-      <div className="side-video-list">
-        <img src={thumbnail1} alt="" />
-        <div className="video-info">
-          <h4>animated car</h4>
-          <p>Riolu</p>
-          <p>166k view</p>
-        </div>
-      </div>
-      <div className="side-video-list">
-        <img src={thumbnail1} alt="" />
-        <div className="video-info">
-          <h4>animated car</h4>
-          <p>Riolu</p>
-          <p>166k view</p>
-        </div>
-      </div>
-      <div className="side-video-list">
-        <img src={thumbnail1} alt="" />
-        <div className="video-info">
-          <h4>animated car</h4>
-          <p>Riolu</p>
-          <p>166k view</p>
-        </div>
-      </div>
-      <div className="side-video-list">
-        <img src={thumbnail1} alt="" />
-        <div className="video-info">
-          <h4>animated car</h4>
-          <p>Riolu</p>
-          <p>166k view</p>
-        </div>
-      </div>
-      <div className="side-video-list">
-        <img src={thumbnail1} alt="" />
-        <div className="video-info">
-          <h4>animated car</h4>
-          <p>Riolu</p>
-          <p>166k view</p>
-        </div>
-      </div>
-      <div className="side-video-list">
-        <img src={thumbnail1} alt="" />
-        <div className="video-info">
-          <h4>animated car</h4>
-          <p>Riolu</p>
-          <p>166k view</p>
-        </div>
-      </div>
-      <div className="side-video-list">
-        <img src={thumbnail1} alt="" />
-        <div className="video-info">
-          <h4>animated car</h4>
-          <p>Riolu</p>
-          <p>166k view</p>
-        </div>
-      </div>
+      {apiData && apiData.map((item, index) => (
+        <Link to={`/video/${item.snippet.categoryId}/${item.id}`} key={index} className="side-video-list">
+          <img src={item.snippet.thumbnails.medium.url} alt="" />
+          <div className="video-info">
+            <h4>{item.snippet.title}</h4>
+            <p>{item.snippet.channelTitle}</p>
+            <p>{value_converter(item.snippet.viewCount)} view</p>
+          </div>
+        </Link>
+      ))}
+
     </div>
-  )
+  );
 }
 
 export default Recommended;
--- src/components/videoPlay/videoPlay.jsx
+++ src/components/videoPlay/videoPlay.jsx
@@ -1,31 +1,60 @@
 import React, { useEffect, useState } from "react";
-import animatedCar from '../../assets/Video/car.mp4';
 import share from '../../assets/Icons/share.png';
 import like from '../../assets/Icons/like.png';
 import dislike from '../../assets/Icons/dislike.png';
 import download from '../../assets/Icons/download.png';
-import profile_icon from '../../assets/Icons/profile.png';
-import riolu from "../../assets/Images/riolu.png";
 import './videoPlay.css';
 import { API_KEY, value_converter } from "../../data";
 import moment from "moment";
-const VideoPlay=({videoId})=>{
+import { useParams } from 'react-router-dom';
 
-  const [apiData,setApiData] = useState(null);
+const VideoPlay = () => {
+  const { videoId } = useParams();
+  const [apiData, setApiData] = useState(null);
+  const [channelData, setChannelData] = useState(null);
+  const [commentData, setCommentData] = useState(null);
 
-  const fetchVideoData = async()=>{
-    const videoDtails_url =`https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=${videoId}&key=${API_KEY}`;
-    await fetch(videoDtails_url).then(res=>res.json()).then(data =>setApiData(data.items[0]));
-  }
+  const fetchVideoData = async () => {
+    const videoDtails_url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=${videoId}&key=${API_KEY}`;
+    await fetch(videoDtails_url)
+      .then((res) => res.json())
+      .then((data) => setApiData(data.items[0]));
+  };
 
-  useEffect(()=>{
-    fetchVideoData()
-  },[]);
+  const fetchChannelData = async () => {
+    if (apiData) {
+      const channelData_url = `https://youtube.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&id=${apiData.snippet.channelId}&key=${API_KEY}`;
+      await fetch(channelData_url)
+        .then((res) => res.json())
+        .then((data) => setChannelData(data.items[0]));
+    }
+  };
+
+  const fetchVideoComment = async () => {
+    if (apiData) {
+      const videoComment_url = `https://youtube.googleapis.com/youtube/v3/commentThreads?part=snippet%2Creplies&maxResults=50&videoId=${videoId}&key=${API_KEY}`;
+      await fetch(videoComment_url)
+        .then((res) => res.json())
+        .then((data) => setCommentData(data.items));
+    }
+  };
+
+  useEffect(() => {
+    fetchVideoData();
+  }, [videoId]);
+
+  useEffect(() => {
+    if (apiData) {
+      fetchChannelData();
+      fetchVideoComment();
+    }
+  }, [apiData]);
+
 
   return(
     <div className="play-video">
        {/*<video src={animatedCar} controls autoPlay muted></video>*/}
-       <iframe src={`https://www.youtube.com/embed/${videoId}?autoplay=1`} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+       <iframe src={`https://www.youtube.com/embed/${videoId}?autoplay=1`} frameByorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
        <h3>{apiData?apiData.snippet.title:"title here"}</h3>
        <div className="play-video-info">
           <p>
@@ -40,10 +69,10 @@ const VideoPlay=({videoId})=>{
        </div>
        <hr />
        <div className="publisher">
-        <img src={riolu} alt="Riolu" />
+        <img src={channelData?channelData.snippet.thumbnails.default.url:""} alt="Publisher" />
         <div>
           <p>{apiData?apiData.snippet.channelTitle:""}</p>
-          <span>85K Subscribers</span>
+          <span>{channelData?value_converter(channelData.statistics.subscriberCount):"1M"} Subscribers</span>
         </div>
         <button>Subscribe</button>
        </div>
@@ -51,39 +80,22 @@ const VideoPlay=({videoId})=>{
           <p>{apiData?apiData.snippet.description.slice(0,600):"Description"}</p>
           <hr />
           <h4>{apiData?value_converter(apiData.statistics.commentCount):111} Comments</h4>
-          <div className="comment">
-            <img src={profile_icon} alt="Profile Icon" />
-            <div>
-              <h3>Heracross <span>1 day ago</span></h3>
-              <p>It's a great animated video</p>
-              <div className="comment-action">
-                <img src={like} alt="Like" /><span>1212</span>
-                <img src={dislike} alt="Dislike" /><span>12</span>
+          {commentData && commentData.map((item,index)=>{
+            return(
+              <div key={index} className="comment">
+                <img src={item.snippet.topLevelComment.snippet.authorProfileImageUrl} alt="Profile Icon" />
+                <div>
+                  <h3>{item.snippet.topLevelComment.snippet.authorDisplayName} <span>1 day ago</span></h3>
+                  <p>{item.snippet.topLevelComment.snippet.textDisplay}</p>
+                  <div className="comment-action">
+                    <img src={like} alt="Like" /><span>{value_converter(item.snippet.topLevelComment.snippet.likeCount)}</span>
+                    <img src={dislike} alt="Dislike" /><span>12</span>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
-          <div className="comment">
-            <img src={profile_icon} alt="Profile Icon" />
-            <div>
-              <h3>Heracross <span>1 day ago</span></h3>
-              <p>It's a great animated video</p>
-              <div className="comment-action">
-                <img src={like} alt="Like" /><span>1212</span>
-                <img src={dislike} alt="Dislike" /><span>12</span>
-              </div>
-            </div>
-          </div>
-          <div className="comment">
-            <img src={profile_icon} alt="Profile Icon" />
-            <div>
-              <h3>Heracross <span>1 day ago</span></h3>
-              <p>It's a great animated video</p>
-              <div className="comment-action">
-                <img src={like} alt="Like" /><span>1212</span>
-                <img src={dislike} alt="Dislike" /><span>12</span>
-              </div>
-            </div>
-          </div>
+            );
+          })}
+          
         </div>
     </div>
   );
